67f27b9299aa39b5aa7e6423387434dc70c8cacc
bofmar <bofmar@outlook.com>
2022-08-11 18:51:34 +0300

    Add basic styling to the banner

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import banner from "./banner.js";
+import "./styles/reset.css";
+import "./styles/style.css";
 
 (function initial(){
   const content = document.getElementById("content");
   console.log(content);
-
-
-
   content.appendChild(banner());
 })();
