af304983622b1ba76024ec3fa8fd3316e8b65471
bofmar <bofmar@outlook.com>
2022-08-13 18:12:56 +0300

    Add logic for the generation of the info side

--- src/contact.js
+++ src/contact.js
@@ -5,6 +5,7 @@ export default function contactPage(){
   const content = document.createElement("div");
   content.classList.add("contact-page");
 
+  // Address side
   const addressSide = document.createElement("div");
   addressSide.classList.add("contact-address-wrapper");
   
@@ -17,15 +18,34 @@ export default function contactPage(){
 
   createAndAppendImage(Map,addressSide, "map");
 
+  // Info side
   const infoSide = document.createElement("div");
   infoSide.classList.add("contact-info-wrapper");
 
-  const footer = document.createElement("footer");
+  const hoursText = createDivWithClass("hours-wrapper")
+  createAndAppendText("h1", hoursText, "Working Hours");
+  createAndAppendText("p", hoursText, "Tuesday: 12:00 - 22:00");
+  createAndAppendText("p", hoursText, "Wednesday: 12:00 - 22:00");
+  createAndAppendText("p", hoursText, "Thursday: 12:00 - 22:00");
+  createAndAppendText("p", hoursText, "Friday: 12:00 - 23:30");
+  createAndAppendText("p", hoursText, "Saturday: 12:00 - 23:30");
+  createAndAppendText("p", hoursText, "Sunday: 12:00 - 23:30");
+  infoSide.appendChild(hoursText);
 
+  const contactDetails = createDivWithClass("contact-details");
+  createAndAppendText("h1", contactDetails,"Contact Us");
+  createAndAppendText("p", contactDetails,"Phone Number: +357 12345678");
+  createAndAppendText("p", contactDetails,"Email: samisen@fakemail.not");
+  infoSide.appendChild(contactDetails);
 
   content.appendChild(addressSide);
   content.appendChild(infoSide);
-  content.appendChild(footer);
 
   return content;
 }
+
+
+
+
+
+
