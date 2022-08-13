df7cc1d7514d39033ffa5fe40c2ce7a42eda7a0c
bofmar <bofmar@outlook.com>
2022-08-13 18:47:18 +0300

    Add logic for switching the pages

--- src/index.js
+++ src/index.js
@@ -5,8 +5,40 @@ import contactPage from "./contact.js";
 import "./styles/reset.css";
 import "./styles/style.css";
 
+let currentPage = "HOME";
+const content = document.getElementById("content");
+
 (function initial(){
-  const content = document.getElementById("content");
   content.appendChild(banner());
   content.appendChild(homepage());
 })();
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => {
+  button.addEventListener("click", switchPage);
+});
+
+function switchPage(e){
+  const nextPage = e.target.innerText;
+  
+  if(nextPage === currentPage) return;
+
+  // detach the current page
+  content.removeChild(content.lastChild);
+
+  // attach new child
+  switch(nextPage){
+    case "HOME":
+      content.appendChild(homepage());
+      break;
+    case "MENU":
+      content.appendChild(menuPage());
+      break;
+    case "CONTACT":
+      content.appendChild(contactPage());
+      break;
+  }
+
+  // update currentPage
+  currentPage = nextPage;
+}
