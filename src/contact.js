ca44803b62a276e93eb05974d335e1ae30f3c243
bofmar <bofmar@outlook.com>
2022-08-13 18:02:06 +0300

    Add code to generate the address and map side

--- src/contact.js
+++ src/contact.js
@@ -1,9 +1,21 @@
+import { createAndAppendText, createAndAppendImage, createDivWithClass } from "./helpers";
+import Map from "./assets/img/paphos-map.png";
+
 export default function contactPage(){
   const content = document.createElement("div");
   content.classList.add("contact-page");
 
   const addressSide = document.createElement("div");
   addressSide.classList.add("contact-address-wrapper");
+  
+  const addressTextDiv = createDivWithClass("address-text-wrapper");
+  createAndAppendText("h1", addressTextDiv, "Address");
+  createAndAppendText("p", addressTextDiv, "123 Fake Street Avenue");
+  createAndAppendText("p", addressTextDiv, "Paphos, CY");
+  createAndAppendText("p", addressTextDiv, "66357");
+  addressSide.appendChild(addressTextDiv);
+
+  createAndAppendImage(Map,addressSide, "map");
 
   const infoSide = document.createElement("div");
   infoSide.classList.add("contact-info-wrapper");
