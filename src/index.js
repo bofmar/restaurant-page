a175725507f74d20b7a65c0e0544123cca39adbe
bofmar <bofmar@outlook.com>
2022-08-13 17:56:39 +0300

    Change index.js to show contact page by default

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import banner from "./banner.js";
 import homepage from "./homepage.js";
 import menuPage from "./menu.js";
+import contactPage from "./contact.js";
 import "./styles/reset.css";
 import "./styles/style.css";
 
 (function initial(){
   const content = document.getElementById("content");
   content.appendChild(banner());
-  content.appendChild(menuPage());
+  content.appendChild(contactPage());
 })();
